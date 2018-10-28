21c77cd8828dc6cd73e79e882dd860d8b658f890
calvinsoul <calvinsoul@gmail.com>
2018-10-27 22:37:57 -0700

    Improved Division Designer + various tweaks

---
+++ common/defines/idd_defines.lua
@@ -0,0 +1,4 @@
+NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 5;		-- Max width of regiments in division designer.
+NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 5;		-- Max height of regiments in division designer.
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2;		-- Max width of support in division designer.
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 5;		-- Max height of support in division designer.
