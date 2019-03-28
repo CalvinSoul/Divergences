c68a2e3fd3d38a532c5ddc68b0cb880a7de41b5a
calvinsoul <calvinsoul@gmail.com>
2019-03-28 03:31:53 -0700

    Building, minister, defines, state-categories, history, all sorts of misc memery

--- common/defines/DIV_game.lua
+++ common/defines/DIV_game.lua
@@ -6,15 +6,15 @@ NDefines.NGame.END_DATE = "1950.1.1.1"
 NDefines.NCountry.WAR_SUPPORT_OFFNSIVE_WAR = -0.05				-- Impact of being in offensive war
 NDefines.NCountry.WAR_SUPPORT_DEFENSIVE_WAR = 0.1				-- Impact of being in defensive war
 NDefines.NCountry.WAR_SUPPORT_TENSION_IMPACT = 0.1			-- Total impact of world tension
-NDefines.NCountry.MIN_STABILITY = -1.0
+NDefines.NCountry.MIN_STABILITY = -0.1
 NDefines.NCountry.MAX_STABILITY = 1.0
-NDefines.NCountry.MIN_WAR_SUPPORT = -1.0
+NDefines.NCountry.MIN_WAR_SUPPORT = -0.1
 NDefines.NCountry.MAX_WAR_SUPPORT = 1.0
 
-NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 25 --Vanillia is 15
-NDefines.NProduction.BASE_FACTORY_SPEED = 2.5 --Vanilla is 5
+NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 20 --Vanillia is 15
+NDefines.NProduction.BASE_FACTORY_SPEED = 3 --Vanilla is 5
 
-NDefines.NPolitics.ARMY_LEADER_COST = 2500
+NDefines.NPolitics.ARMY_LEADER_COST = 10
 
 NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 24		-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
 NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1		-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
@@ -23,3 +23,5 @@ NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5
 
 NDefines.NFocus.FOCUS_POINT_DAYS = 1
 NDefines.NBuildings.MAX_SHARED_SLOTS = 50
+
+NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 1
